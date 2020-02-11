1d5c6ab51926cc10dd38062a07940e7ae0f29457
Shemshad Amiri Khorasani <shemshadamirikhorasani@Shemshads-MacBook-Pro.local>
2020-02-11 16:48:05 +0330

    welcome

--- panel/index.php
+++ panel/index.php
@@ -1,1 +1,14 @@
 <?php
+$url = "http://userpanel.lc/";
+if (isset($_COOKIE)){
+    echo "به پنل خود خوش آمدید";
+    echo "<br>";
+    echo "رمز جدید هر روز برای شما ارسال می شود";
+    echo "<br>";
+    echo "این نشست تا 6000 ثانیه دیگر برای شما منقضی می شود :)";
+
+
+}
+else {
+    header("Location: $url");
+}
